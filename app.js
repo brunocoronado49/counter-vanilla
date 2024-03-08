d8b30c589480be1832648722171f4822febc943d
Francisco Rangel <francorangelcoronado@gmail.com>
2024-03-08 00:41:04 -0600

    creando contador con estilos copiados

---
+++ app.js
@@ -0,0 +1,30 @@
+let count = 0;
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(btn => {
+  btn.addEventListener('click', (event) => {
+    const styles = event.target.classList;
+
+    if (styles.contains('decrease')) {
+      count--;
+    } else if (styles.contains('increase')) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+
+    value.innerText = count;
+  });
+});
